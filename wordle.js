1d77297ce4d7f3115e82515eefe912bada647272
Tulasikrishna <tulasikrishnabudda55@gmail.com>
2024-03-02 17:49:48 +0530

    once the submit is clicked the result won't change again

--- wordle.js
+++ wordle.js
@@ -2,10 +2,14 @@ let userInput = document.getElementById("userInput");
 let button = document.getElementById("submit");
 let result = document.getElementById("result");
 let randomLetter = "APPLE";
+let value = true;
 button.onclick = function () {
-  if (userInput.value.toUpperCase() === randomLetter) {
-    result.textContent = "YOU WON!";
-  } else {
-    result.textContent = `YOU LOSE,the correct word is ${randomLetter}`;
+  if (value) {
+    if (userInput.value.toUpperCase() === randomLetter) {
+      result.textContent = "YOU WON!";
+    } else {
+      result.textContent = `YOU LOSE,the correct word is ${randomLetter}`;
+    }
+    value = false;
   }
 };
