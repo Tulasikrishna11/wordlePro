44d2f004d1a6f0b7fbeffdf2674d886dfc904235
Tulasikrishna <tulasikrishnabudda55@gmail.com>
2024-03-02 18:51:44 +0530

    added new functionality tries left

--- wordle.js
+++ wordle.js
@@ -1,24 +1,26 @@
 let userInput = document.getElementById("userInput");
 let button = document.getElementById("submit");
 let result = document.getElementById("result");
-let container = document.getElementById("mainContainer");
+let container = document.getElementById("wordContainer");
 let randomLetter = "APPLE";
 let value = true;
-let count = 0;
+let count = 2;
+let triesLeft = document.getElementById("triesLeft");
 button.onclick = function () {
-  if (count < 2) {
-    if (value) {
-      if (userInput.value.toUpperCase() === randomLetter) {
-        result.textContent = "YOU WON!";
-      } else {
-        let word = document.createElement("p");
-        word.textContent = userInput.value.toUpperCase();
-        container.appendChild(word);
+  if (count > 0) {
+    if (userInput.value.toUpperCase() === randomLetter) {
+      result.textContent = "YOU WON!";
+      userInput.value = "";
+    } else {
+      let word = document.createElement("p");
+      word.textContent = userInput.value.toUpperCase();
+      container.appendChild(word);
+      userInput.value = "";
+      if (count == 1) {
+        result.textContent = `YOU LOSE,the correct word is ${randomLetter}`;
       }
-      count += 1;
     }
-  } else {
-    value = false;
-    result.textContent = `YOU LOSE,the correct word is ${randomLetter}`;
+    count -= 1;
+    triesLeft.textContent = "Tries Left =" + count;
   }
 };
