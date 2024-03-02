c1cce7e4b86ff7a0cabea2afec6bb4f28086a870
Tulasikrishna <tulasikrishnabudda55@gmail.com>
2024-03-02 18:25:05 +0530

    increasing the no of tries  to 2

--- wordle.js
+++ wordle.js
@@ -1,15 +1,24 @@
 let userInput = document.getElementById("userInput");
 let button = document.getElementById("submit");
 let result = document.getElementById("result");
+let container = document.getElementById("mainContainer");
 let randomLetter = "APPLE";
 let value = true;
+let count = 0;
 button.onclick = function () {
-  if (value) {
-    if (userInput.value.toUpperCase() === randomLetter) {
-      result.textContent = "YOU WON!";
-    } else {
-      result.textContent = `YOU LOSE,the correct word is ${randomLetter}`;
+  if (count < 2) {
+    if (value) {
+      if (userInput.value.toUpperCase() === randomLetter) {
+        result.textContent = "YOU WON!";
+      } else {
+        let word = document.createElement("p");
+        word.textContent = userInput.value.toUpperCase();
+        container.appendChild(word);
+      }
+      count += 1;
     }
+  } else {
     value = false;
+    result.textContent = `YOU LOSE,the correct word is ${randomLetter}`;
   }
 };
