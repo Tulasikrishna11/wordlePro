7dc8948107a030a0b0a760617cd14432d8327bc2
Tulasikrishna <tulasikrishnabudda55@gmail.com>
2024-03-04 23:07:25 +0530

    code refactor

--- wordle.js
+++ wordle.js
@@ -2,16 +2,16 @@ let userInput = document.getElementById("userInput");
 let button = document.getElementById("submit");
 let result = document.getElementById("result");
 let container = document.getElementById("wordsContainer");
-let randomLetter = "GUARD";
-let count = 5;
-let memory = "";
 let triesLeft = document.getElementById("triesLeft");
-function win() {
-  result.textContent = randomLetter + " is the correct word YOU WON! ";
+let randomWord = "GUARD";
+let count = 5;
+function declareWin() {
+  result.textContent = randomWord + " is the correct word YOU WON! ";
   button.disabled = true;
   triesLeft.textContent = "";
 }
 function displayUserInput() {
+  let memory = "";
   let wordContainer = document.createElement("div");
   wordContainer.classList.add("word-container");
   container.appendChild(wordContainer);
@@ -22,7 +22,7 @@ function displayUserInput() {
     console.log(`${count}${i}`);
     letterContainer.textContent = userEnteredWord[i];
     wordContainer.appendChild(letterContainer);
-    if (userEnteredWord[i] === randomLetter[i]) {
+    if (userEnteredWord[i] === randomWord[i]) {
       letterContainer.style.backgroundColor = "green";
       memory += userEnteredWord[i];
     }
@@ -34,7 +34,7 @@ function displayUserInput() {
     if (color !== "green") {
       if (
         !memory.includes(userEnteredWord[i]) &&
-        randomLetter.includes(userEnteredWord[i])
+        randomWord.includes(userEnteredWord[i])
       ) {
         letterContainer.style.backgroundColor = "yellow";
       } else {
@@ -44,8 +44,8 @@ function displayUserInput() {
   }
 }
 function lose() {
-  if (count == 0) {
-    result.textContent = `YOU LOSE,the correct word is ${randomLetter}`;
+  if (count === 0) {
+    result.textContent = `YOU LOSE,the correct word is ${randomWord}`;
     button.disabled = true;
     triesLeft.textContent = "";
   }
@@ -54,21 +54,20 @@ function displayTriesLeft() {
   triesLeft.textContent = "Tries Left =" + count;
 }
 button.onclick = function () {
-  if (userInput.value.length === 5) {
-    if (count > 0) {
-      memory = "";
-      if (userInput.value.toUpperCase() === randomLetter) {
-        win();
-        displayUserInput();
-      } else {
-        displayUserInput();
-        count -= 1;
-        displayTriesLeft();
-        lose();
-      }
-      userInput.value = "";
-    }
-  } else {
+  let userInputValue = userInput.value.toUpperCase();
+  if (count < 1) return;
+  if (userInputValue.length !== 5) {
     alert("enter a five letter word");
+    return;
   }
+
+  if (userInputValue === randomWord) {
+    declareWin();
+  } else {
+    count -= 1;
+    displayTriesLeft();
+    lose();
+  }
+  displayUserInput();
+  userInput.value = "";
 };
